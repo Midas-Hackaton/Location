1ad98cb253d846dc879db6772021d0fa8f92c9d4
eternrust <eternrust@dsm.hs.kr>
2022-11-04 00:20:04 +0900

    ADD :: Header Toggle 추가 및 수정

--- src/components/header/index.style.js
+++ src/components/header/index.style.js
@@ -2,13 +2,17 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
   width: 100%;
+  height: 100px;
   background-color: #6e42ea;
   display: flex;
   justify-content: space-around;
   align-items: center;
 `;
 
-export const LogoContainer = styled.div``;
+export const LogoContainer = styled.div`
+  cursor: pointer;
+`;
+
 export const LoginContainer = styled.div`
   display: flex;
   align-items: center;
@@ -23,3 +27,136 @@ export const LoginContainer = styled.div`
     border-radius: 5px;
   }
 `;
+
+export const Button_box = styled.button`
+  width: 220px;
+  height: 50px;
+  background: #FFFFFF;
+  box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  border: 0;
+  outline: 0;
+  padding: 0 10px;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  transition: 0.3s;
+  &:hover {
+    transform: scale(1.02);
+  }
+`
+
+export const Button_span = styled.span`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+
+  color: #000000;
+`
+
+export const User_box = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const Alert_Image = styled.img`
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  margin-left: 20px;
+`
+
+export const User_Image = styled.img`
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 100px;  
+  box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.25);
+  margin-left: 20px;
+`
+
+export const User_Name = styled.span`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+
+  color: #000000;
+  margin-left: 20px;
+`
+
+export const Toggle_box = styled.div`
+  width: 110px;
+  height: 50px;
+
+  background: #FFFFFF;
+  box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.5);
+  border-radius: 100px;
+  margin-left: 20px;
+  cursor: pointer;
+  
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 23px;
+
+  color: #000000;
+`
+
+export const Triangle = styled.div`
+  width: 0;
+  height: 0;
+  border-top: 20px solid #6E42EA;
+  border-left: 12px solid transparent;
+  border-right: 12px solid transparent;
+
+  transition: 0.3s;
+  transform: rotate(${(props) => props.rotate || '0deg' });
+`
+
+export const Toggle_menu_box = styled.div`
+  width: 150px;
+  height: 205px;
+  background: #FFFFFF;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.5);
+  border-radius: 30px;
+  position: absolute;
+  top: 90px;
+  right: 380px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Toggle_item = styled.div`
+  width: 120px;
+  height: 40.8px;
+  border-bottom: ${(props) => props.bottom || '1px dashed #000000'};
+  padding-left: 10px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 23px;
+
+  color: #000000;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  cursor: pointer;
+`
--- src/components/header/login.js
+++ src/components/header/login.js
@@ -1,8 +1,11 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../../util/auth/firebase";
 import { deleteCookie, getCookie, setCookie } from "../../util/cookie/cookie";
 import { useNavigate } from "react-router-dom";
+import AlertImg from "../../assets/img/logo/alert.png";
+import LogoutImg from "../../assets/img/logo/Logout.png";
+import GoogleLogo from "../../assets/img/logo/Google_Logo.png";
 import * as S from "./index.style"
 function Login() {
   const navigate = useNavigate();
@@ -48,20 +51,51 @@ function Login() {
     }
   }, []);
 
+  const Logout_question = () => {
+    let answer = window.confirm("정말로 로그아웃 하시겠습니까?");
+    if (answer) handleGoogleLogout();
+  }
+
+  const [Data, setData] = useState({
+    data: '기타',
+    rotate: '0deg'
+  });
+
   return (
     <div>
       {isLogin ? (
-        <S.LoginContainer>
-          <img
-            src={userData.photoURL}
-            alt="유저의 프사"
-            style={{ borderRadius: "50%" }}
-          />
-          <h2>{userData && userData.displayName}님</h2>
-          <button onClick={handleGoogleLogout}>로그아웃</button>
-        </S.LoginContainer>
+        <S.User_box>
+          <S.Alert_Image src={AlertImg} />
+          <S.User_Image src={userData.photoURL} alt="유저의 프사" />
+          {userData && <S.User_Name>{userData.displayName}</S.User_Name>}
+          
+          <S.Toggle_box onClick={() => {
+            if (Data.rotate == '180deg') {
+              setData({...Data, rotate: '0deg'});
+            } else {
+              setData({...Data, rotate: '180deg'});
+            }
+          }}>
+            {Data.data}
+            <S.Triangle rotate={Data.rotate} />
+          </S.Toggle_box>
+
+          { Data.rotate == '180deg' &&
+            <S.Toggle_menu_box>
+              <S.Toggle_item onClick={() => setData({data: '퇴근', rotate: '0deg'})}>퇴근</S.Toggle_item>
+              <S.Toggle_item onClick={() => setData({data: '출근 중', rotate: '0deg'})}>출근 중</S.Toggle_item>
+              <S.Toggle_item onClick={() => setData({data: '회의 중', rotate: '0deg'})}>회의 중</S.Toggle_item>
+              <S.Toggle_item onClick={() => setData({data: '휴식 중', rotate: '0deg'})}>휴식 중</S.Toggle_item>
+              <S.Toggle_item bottom='none' onClick={() => setData({data: '기타', rotate: '0deg'})}>기타</S.Toggle_item>
+            </S.Toggle_menu_box>
+          }
+          <S.Alert_Image src={LogoutImg} onClick={() => Logout_question()} alt="로그아웃" />
+        </S.User_box>
       ) : (
-        <button onClick={handleGoogleLogin}>로그인</button>
+        <S.Button_box onClick={handleGoogleLogin}>
+          <img src={GoogleLogo} width='35px' height='35px' />
+          <S.Button_span>Google로 로그인하기</S.Button_span>
+        </S.Button_box>
       )}
     </div>
   );
