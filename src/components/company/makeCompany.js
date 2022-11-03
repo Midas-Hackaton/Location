08f6f771728cc7014338f4cd8d3592630b694ef5
J1min <aoiharu41@gmail.com>
2022-11-03 16:40:42 +0900

    ADD :: 새회사 등록 추가

---
+++ src/components/company/makeCompany.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
+import { v4 as uuidv4 } from "uuid";
+
+function MakeCompany() {
+  const db = getDatabase();
+
+  const companyRef = ref(db, `company/${uuidv4()}`);
+
+  const setData = () => {
+    set(companyRef, {
+      companyName: companyName,
+    });
+  };
+
+  const [companyName, setCompanyName] = React.useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setCompanyName(e.target.value);
+        }}
+      />
+      <button onClick={setData}>회사 등록</button>
+    </div>
+  );
+}
+
+export default MakeCompany;
