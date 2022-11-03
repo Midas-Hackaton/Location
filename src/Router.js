07f18b3f88b9d768c0984a453117109d233e83e4
GyoungMin <101560583+Bae0203@users.noreply.github.com>
2022-11-03 11:55:41 +0900

    myinfo 추가
    myinfo 추가
    라우팅 추가

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Main from "./Components/Main";
+import Myinfo from "./Components/myinfo";
 
 const router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/myinfo" element={<Myinfo />} />
       </Routes>
     </BrowserRouter>
   );
